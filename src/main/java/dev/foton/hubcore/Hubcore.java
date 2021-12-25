a50baa280cdfd1048bca3e3cbf09fda0668aef1b
nitkanikita21 <waleryvvs1@gmail.com>
2021-12-25 15:17:34 +0200

    modify gradle

--- src/main/java/dev/foton/hubcore/Hubcore.java
+++ src/main/java/dev/foton/hubcore/Hubcore.java
@@ -1,5 +1,6 @@
 package dev.foton.hubcore;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Hubcore extends JavaPlugin {
@@ -7,6 +8,10 @@ public final class Hubcore extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        getServer().getLogger().info(ChatColor.GREEN+"[!!!] SERVER READY TO WORK");
+        getServer().getLogger().info(ChatColor.GREEN+"[!!!] SERVER READY TO WORK");
+        getServer().getLogger().info(ChatColor.GREEN+"[!!!] SERVER READY TO WORK");
 
     }
 
