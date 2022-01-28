e527fec285fe73a972ec9c0708ec082811856546
Youat0m <senkaved@gmail.com>
2022-01-28 18:52:40 +0300

    переписал некоторые кнопки на OpenMenu

--- src/main/java/dev/foton/hubcore/Main.java
+++ src/main/java/dev/foton/hubcore/Main.java
@@ -10,6 +10,7 @@ import dev.foton.hubcore.modules.commands.CustomCommandBuilder;
 import dev.foton.hubcore.modules.interfaces.MenuListener;
 import dev.foton.hubcore.modules.interfaces.MenuManager;
 import dev.foton.hubcore.modules.interfaces.items.Text;
+import dev.foton.hubcore.modules.interfaces.items.sub.OpenMenu;
 import dev.foton.hubcore.modules.interfaces.items.sub.ScriptableButton;
 import dev.foton.hubcore.modules.interfaces.menu.ChestMenu;
 import dev.foton.hubcore.modules.interfaces.menu.DispancerMenu;
@@ -70,8 +71,7 @@ public final class Main extends JavaPlugin {
 
         new CustomCommandBuilder()
                 .name("spawnnpc").executor((commandSender, strings) -> {
-            if (commandSender instanceof Player){
-                Player p = (Player) commandSender;
+            if (commandSender instanceof Player p){
                 NPCManager.getInstance().appendGameNPC(p.getLocation(),strings[0],player -> {
                     p.sendMessage("test");
                     ServerConnectionManager.connect(player,strings[1]);
@@ -82,7 +82,6 @@ public final class Main extends JavaPlugin {
         new CustomCommandBuilder()
                 .name("hats").executor((commandSender, strings) -> {
             if (commandSender instanceof Player){
-                Player p = (Player) commandSender;
                 MenuManager.open((Player) commandSender,MenuManager.getMenu("hats"));
             }
         }).registryPlugin();
@@ -105,7 +104,7 @@ public final class Main extends JavaPlugin {
         ScriptableButton campfireBtn = new ScriptableButton(
                 Material.CAMPFIRE,
                 "&7Режим "+new SolidTextBuilder().color("#42f5a4").text("Campfire").build().getJsonText(),
-                "campfireBtn", new ArrayList<>(), new Vector(2,2,1),
+                "campfireBtn", new Vector(2,2,1),
                 1
         ){
             @Override
@@ -128,35 +127,24 @@ public final class Main extends JavaPlugin {
 
         ChestMenu allMenus = new ChestMenu("&6Все меню", "menu_all_menu",3);
 
-        ScriptableButton goToGame = new ScriptableButton(
+        OpenMenu goToGame = new OpenMenu(
                 Material.COMPASS,
                 "&6Все сервера",
-                "allGamesBtn", new ArrayList<>(), new Vector(5,2,1),
-                1
-        ){
-            @Override
-            public void OnUse(InventoryClickEvent e){
-                MenuManager.open((Player) e.getWhoClicked(), MenuManager.getMenu("menu_go_to"));
-            }
-        };
-        ScriptableButton hatsBtn = new ScriptableButton(
+                "allGamesBtn", new Vector(5,2,1),
+                1, "menu_go_to"
+        );
+        OpenMenu hatsBtn = new OpenMenu(
                 Material.LEATHER_HELMET,
                 "&bШапки",
-                "hatsBtn", new ArrayList<>(), new Vector(3,2,1),
-                1
-        ){
-
-            @Override
-            public void OnUse(InventoryClickEvent e) {
-                MenuManager.open((Player) e.getWhoClicked(),MenuManager.getMenu("hats"));
-            }
-        };
+                "hatsBtn", new Vector(3,2,1),
+                1, "hats"
+        );
 
 
         ScriptableButton settings = new ScriptableButton(
                 Material.COMPARATOR,
                 "&bНастройки",
-                "settingsBtn", new ArrayList<>(), new Vector(7,2,1),
+                "settingsBtn", new Vector(7,2,1),
                 1
         ){
             @Override
@@ -173,8 +161,7 @@ public final class Main extends JavaPlugin {
 
         int x = 1;
         int y = 1;
-        List<HatsCollection> collections = new ArrayList<>();
-        collections.addAll(HatsManager.getCollectionMap().values());
+        List<HatsCollection> collections = new ArrayList<>(HatsManager.getCollectionMap().values());
 
         for (int i = 0; i < collections.size() && y < hatsCollectionsMenu.getHeight(); i++) {
             if(x > 9) {
@@ -186,7 +173,7 @@ public final class Main extends JavaPlugin {
 
             ScriptableButton h = new ScriptableButton(collection.getIcon(),
                     collection.getName(),
-                    collection.getId(),new ArrayList<>(),
+                    collection.getId(),
                     new Vector(x,y,1),1
             ){
 
@@ -211,7 +198,7 @@ public final class Main extends JavaPlugin {
 
                         Text hatText = new Text(hat.getIcon(),
                                 new SolidTextBuilder().text(hat.getName()).color(hat.getColorName()).build().getJsonText(),
-                                hat.getId(),new ArrayList<>(),
+                                hat.getId(),
                                 new Vector(x2,y2,1),1
                         );
 
